d5319972f54a97ff1fb9e161b37a2a9bc1196854
Akhilesh Macbook <akhileshmacbook@Akhileshs-MacBook-Air.local>
2022-02-25 11:50:11 +0530

    first commit

---
+++ src/Data/Categories.js
@@ -0,0 +1,9 @@
+const Categories = [
+  {
+    category: "General Knowledge",
+    value: 9
+  },
+   { category: "Geography", value: 22 },
+ ];
+
+export default Categories;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      <a
+        href="/https://github.com/satisshpilla/Quiz_using_api"
+        style={{ cursor: "pointer", 
+                  color:"green"}}
+      >
+      click here
+      </a>
+      {""} for code
+    </div>
+  );
+};
+
+export default Footer;
